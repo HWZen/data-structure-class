d3581ee1b5c1096f5722dae9b6f4a19e04341ae3
HWZen <46065109+HWZen@users.noreply.github.com>
2020-11-29 16:34:53 +0800

    add GFWlist.txt and Linear Algebra project

--- Linear Algebra and Its Applications/Matrix.cpp
+++ Linear Algebra and Its Applications/Matrix.cpp
@@ -1,28 +1,16 @@
 #include "Matrix.h"
+#include <string>
+using namespace std;
 
-Matrix::Matrix(int xnum, int ynum)
+int Matrix::GetRankNum()
 {
-    Xlen = xnum;
-    Ylen = ynum;
-    array = new double *[ynum];
-    for (int i = 0; i < ynum; i++)
-        array[ynum] = new double[xnum];
+    if(Rank_Num!=-1)
+        return Rank_Num;
+    Matrix temp(*this);
+
 }
 
-Matrix::Matrix(int xnum, int ynum, double **ARRAY)
-{
-    Xlen = xnum;
-    Ylen = ynum;
-    array = new double *[ynum];
-    for (int i = 0; i < ynum; i++)
-    {
-        array[i] = new double[xnum];
-        for (int j = 0; j < xnum; j++)
-            array[i][j] = ARRAY[i][j];
-    }
-}
-
-Matrix LU(Matrix mat)
+Matrix Matrix::LU(Matrix mat)
 {
     double **anser;
     double *temp;
@@ -30,4 +18,39 @@ Matrix LU(Matrix mat)
     anser = new double *[mat.Ylen];
     for (int i = 0; i < mat.Ylen; i++)
         anser[i] = new double[mat.Xlen];
+}
+
+Matrix& Matrix::operator+=(const Matrix &mat)
+{
+    Matrix anser(Xlen > mat.Xlen ? Xlen : mat.Xlen, Ylen > mat.Ylen ? Ylen : mat.Ylen);
+
+    for (int i = 0; i < Xlen; i++)
+        for (int j = 0; j < Ylen; j++)
+            anser.array[i][j] = array[i][j];
+
+    for (int i = 0; i < mat.Xlen; i++)
+        for (int j = 0; j < mat.Ylen; j++)
+            anser.array[i][j] += mat.array[i][j];
+
+    return anser;
+}
+
+Matrix &Matrix::operator-=(const Matrix &mat)
+{
+    Matrix anser(Xlen > mat.Xlen ? Xlen : mat.Xlen, Ylen > mat.Ylen ? Ylen : mat.Ylen);
+
+    for (int i = 0; i < Xlen; i++)
+        for (int j = 0; j < Ylen; j++)
+            anser.array[i][j] = array[i][j];
+
+    for (int i = 0; i < mat.Xlen; i++)
+        for (int j = 0; j < mat.Ylen; j++)
+            anser.array[i][j] -= mat.array[i][j];
+
+    return anser;
+}
+
+Matrix Matrix::Row_Echelon()
+{
+    
 }
--- Linear Algebra and Its Applications/Matrix.h
+++ Linear Algebra and Its Applications/Matrix.h
@@ -5,21 +5,54 @@ class Matrix
 {
 private:
     double **array;
-    int Xlen;
-    int Ylen;
-    friend Matrix LU(Matrix mat);
+    int Xlen;// 行数
+    int Ylen;// 列数
+    int Rank_Num;// 秩
 
 public:
     Matrix();
     Matrix(const Matrix &mat);
     Matrix(int Xnum, int Ynum);
     Matrix(int Xnum, int Ynum, double **ARRAY);
+    Matrix &operator+=(const Matrix &); //矩阵的+=操作
+    Matrix &operator-=(const Matrix &); //-=
+
+    Matrix Row_Echelon(); // 梯形矩阵
+
+    int GetRankNum(); // 获取秩
+
+    Matrix LU(Matrix mat); // LU分解
     ~Matrix();
 };
 Matrix::Matrix()
 {
     array = NULL;
     Xlen = Ylen = 0;
+    Rank_Num = -1;
+}
+
+Matrix::Matrix(int xnum, int ynum)
+{
+    Xlen = xnum;
+    Ylen = ynum;
+    Rank_Num = -1;
+    array = new double *[ynum];
+    for (int i = 0; i < ynum; i++)
+        array[ynum] = new double[xnum];
+}
+
+Matrix::Matrix(int xnum, int ynum, double **ARRAY)
+{
+    Xlen = xnum;
+    Ylen = ynum;
+    Rank_Num = -1;
+    array = new double *[ynum];
+    for (int i = 0; i < ynum; i++)
+    {
+        array[i] = new double[xnum];
+        for (int j = 0; j < xnum; j++)
+            array[i][j] = ARRAY[i][j];
+    }
 }
 
 Matrix::~Matrix()
@@ -27,4 +60,13 @@ Matrix::~Matrix()
     for (int i = 0; i < Ylen; i++)
         delete[] array[i];
     delete array;
+}
+
+Matrix::Matrix(const Matrix &mat)
+{
+    Matrix(mat.Xlen, mat.Ylen);
+
+    for (int i = 0; i < Xlen; i++)
+        for (int j = 0; j < Ylen; j++)
+            array[i][j] = mat.array[i][j];
 }
